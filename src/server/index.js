0b397b24fc2992e6cc22904a2a3387bca5709448
RafaelGSS <rafael.nunu@hotmail.com>
2019-10-06 22:59:10 -0300

    feat: base implementation of server

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const fs = require('fs')
 const path = require('path')
 const fastify = require('fastify')({
   logger: { level: 'info' },
-  // http2: true,
+  http2: true,
   https: {
     key: fs.readFileSync(path.join(__dirname, 'localhost.key')),
     cert: fs.readFileSync(path.join(__dirname, 'localhost.crt'))
@@ -11,7 +13,20 @@ const fastify = require('fastify')({
 
 const oauthPlugin = require('fastify-oauth2')
 
-fastify.register(oauthPlugin, require('./config'))
+fastify.register(oauthPlugin, {
+  name: 'facebookOAuth2',
+  credentials: {
+    client: {
+      id: process.env.APP_ID,
+      secret: process.env.APP_SECRET
+    },
+    auth: oauthPlugin.FACEBOOK_CONFIGURATION
+  },
+  // register a fastify url to start the redirect flow
+  startRedirectPath: '/login/facebook',
+  // facebook redirect here after the user login
+  callbackUri: 'http://localhost:3000/login/facebook/callback'
+})
 
 fastify.get('/login/facebook/callback', async function (request, reply) {
   const token = await this.getAccessTokenFromAuthorizationCodeFlow(request)
